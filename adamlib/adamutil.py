1e09990df5113fa29aecc5529fc08999f0e50e65
Joel Stanley <joel@jms.id.au>
2009-02-11 23:01:55 +1030

    Licence and header

--- adamlib/adamutil.py
+++ adamlib/adamutil.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+# GNOME Adam Internet Applet
 # Copyright 2009 Joel Stanley <joel@jms.id.au>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,6 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import urllib2
 from BeautifulSoup import BeautifulSoup as soup
